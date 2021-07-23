73904f3df0fa3a37583005ee851a7dcdede3bb2b
cqrlosArch <diaz.flores.c@gmail.com>
2021-07-23 20:47:14 +0200

    reset form

--- src/common/module.js
+++ src/common/module.js
@@ -5,16 +5,29 @@ const tipAmount = document.getElementById('tip-amount');
 const totalPerson = document.getElementById('total-person');
 const inputsRadio = document.querySelectorAll('.form-tips__input');
 
+//Object of instance
 const tipCalculator = new CalculatorTips();
 
+//Calculate Tips
 const calculateTip = (app) => {
   tipAmount.textContent = `$${app.tipAmountValue().toFixed(2)}`;
   totalPerson.textContent = `$${app.totalPersonValue().toFixed(2)}`;
 };
 
+//unChecked Radio Input
 const unCheckedRadioTip = () =>
   inputsRadio.forEach((input) => (input.checked = false));
 
+//Reset form $0.00
+const resetForm = () => {
+  tipAmount.textContent = `$0.00`;
+  totalPerson.textContent = `$0.00`;
+};
+
+//Event listener Reset form
+form.addEventListener('reset', resetForm);
+
+//Event listener Input form
 form.addEventListener('input', (e) => {
   tipCalculator.setValor([e.target.name], parseFloat(e.target.value));
 
@@ -24,7 +37,6 @@ form.addEventListener('input', (e) => {
   if (tipCalculator.validator()) {
     calculateTip(tipCalculator);
   } else {
-    tipAmount.textContent = `$0.00`;
-    totalPerson.textContent = `$0.00`;
+    resetForm();
   }
 });
